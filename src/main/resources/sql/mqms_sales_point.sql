fc598cd6f887e10bcd429dea522dbf3ca3f4f176
dielianhua <798846080@qq.com>
2020-03-03 15:25:34 +0800

    增加VIN和销售点解码表

--- src/main/resources/sql/mqms_sales_point.sql
+++ src/main/resources/sql/mqms_sales_point.sql
@@ -22,7 +22,7 @@ DROP TABLE IF EXISTS `mqms_sales_point`;
 CREATE TABLE `mqms_sales_point` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `sales_point_name` varchar(255) DEFAULT NULL COMMENT '销售点名称',
-  `sales_point_area` varchar(2000) DEFAULT NULL COMMENT '销售区域',
+  `sales_point_area` varchar(255) DEFAULT NULL COMMENT '销售区域',
   `sales_point_code` varchar(255) DEFAULT NULL COMMENT '销售点代码',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=139580 DEFAULT CHARSET=utf8 COMMENT='销售点区域解析表';
