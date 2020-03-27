59a55a04c2e45c258ad43ebbe32b9439139fcc1b
SamuncleLi <lzhshenqing@126.com>
2020-03-27 16:49:31 +0800

    整合

---
+++ src/main/java/com/gamc/efactory/dao/base/MqmsTransTypeBaseMapper.java
@@ -0,0 +1,21 @@
+package com.gamc.efactory.dao.base;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import com.gamc.efactory.model.dataObject.MqmsTransType;
+/**
+*  @author lizeh
+*/
+public interface MqmsTransTypeBaseMapper {
+
+    int insertMqmsTransType(MqmsTransType object);
+
+    int updateMqmsTransType(MqmsTransType object);
+
+    int update(MqmsTransType.UpdateBuilder object);
+
+    List<MqmsTransType> queryMqmsTransType(MqmsTransType object);
+
+    MqmsTransType queryMqmsTransTypeLimit1(MqmsTransType object);
+
+}
